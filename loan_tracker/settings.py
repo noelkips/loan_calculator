3ec4f1bd7b2ee06adfa3fe1d958a9685ca924cef
Noel Langat <noel.langat@MoHIAfrica.Local>
2025-07-28 14:54:23 +0300

    update settings.py

--- loan_tracker/settings.py
+++ loan_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$n*b8z7$)2y-6t1%4vs+xd*)(tvr4cz@^9%55f42$olsetiluq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
